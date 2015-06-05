5e174711551d3ca8cfee69826327d2190c0ae9e6
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2015-06-05 17:43:48 +0200

    pass through the call for getting table values from rest service

--- nodetest1/routes/services.js
+++ nodetest1/routes/services.js
@@ -16,4 +16,18 @@ router.get('/tags/?*', function(req, res)
 
 });
 
+
+router.get('/keys/?*', function(req, res) 
+{
+  console.log(req['path']);
+  // response.simpleText(200, "Hello World!");
+  request.get({ uri:'http://0.0.0.0:1234'+req['path'], json:true}, function(error,response, body){
+    console.log(body);
+    res.send(body);
+
+  });
+  // res.send(, json: true);
+
+});
+
 module.exports = router;
