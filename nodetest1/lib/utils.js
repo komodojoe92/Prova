f3a9eb9b82cf25b37c73c0bd41900863585844e1
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2015-06-10 11:12:24 +0200

    refactoring moving some logic to the server side.

---
+++ nodetest1/lib/utils.js
@@ -0,0 +1,49 @@
+module.exports = {
+  sayHelloInEnglish: function() {
+    return "HELLO";
+  },
+        
+  sayHelloInSpanish: function() {
+    return "Hola";
+  },
+
+/*
+  Convert an array of results in an hash with key = the name of the file and values the rows of data belonging to head file.
+  */
+   bdfQueryToTables: function(bdfResultJson){
+    var dataTables = {};
+ 
+    // counts the number of results returned by biodatafinder
+    var resultsLength = bdfResultJson['objs'].length;
+    if( resultsLength != 0){
+      //iterate over the objs array which contains {:infos, :data}
+      var filepath = '';
+      
+      var record = undefined;
+      var header_name = undefined;
+      for (i=0 ; i<resultsLength ; i++){
+        record = bdfResultJson.objs[i];
+         filepath = record.infos.filepath;
+        // in case the filepath (key for a dataset) is not present, creates a new array of elements for that key
+        if(dataTables[filepath] == undefined){
+          var h = {};
+          h['data']=[];
+          h['header']=record.infos.header;
+          dataTables[filepath] = h;
+        }
+
+        var row = [];
+
+        row = record['infos']['header'].map(function(header){
+          return record.data[header]
+        })
+
+        dataTables[filepath]['data'].push(row);
+      }
+    } //if bdfResultJson != 0
+    return dataTables;
+  }
+
+}; //module.exports
+
+
--- nodetest1/public/js/ui.js
+++ nodetest1/public/js/ui.js
@@ -56,99 +56,133 @@ google.load('visualization', '1', {packages: ['corechart', 'bar']});
             string += this + '%20'
           })
           string = string.substring(0, string.length-3);
-          $.getJSON(webserver+ string +'/keys/'+ searchCamp, function(data){
-          console.log(data);
-          showTableChart(data);
+          var uri = webserver+ string +'/keys/'+ searchCamp;
+          console.log(uri);
+          $.getJSON( uri, function(data){
+            console.log(data);
+            showTableChart(data);
           })
         }
       }
 
-      function showTableChart(obj){
-        var pippo="";
-        var array;
-        papa = [];
-        if(obj.objs.length == 0){
-          alert('The result is empty! try with other tag(s)');
-          return;
+
+        /*
+  datasetName is the filepath/key to access the record data
+  */
+  function tableToDataTableHeader(dataTable){
+   
+    console.log(dataTable);
+    dataTable.header.map(function(column_name){
+      var h=[];
+      h['title']=column_name;
+      return h;
+    })
+  }
+
+  function tableToDataTableBody(dataTable){
+    dataTable.rows;
+  }
+
+  function tableToDataTable(dataTable){
+    var h = [];
+    h['data']=tableToDataTableBody(dataTable);
+    h['columns']=tableToDataTableHeader(dataTable);
+    return h;
+  }
+
+      function showTableChart(tables){
+
+        createResults(tables);
+      }
+
+
+      function createResults(tables){
+        var keys = Object.keys(tables)
+        console.log(keys);
+        console.log(tables)
+        for (var i = 0; i < keys.length; i++) {
+          var tableName = keys[i];
+          console.log(tableName)
+          var dataTable = tableToDataTable(tables[tableName]);
+          // console.log(dataTable);
+          createResultTab(i, i);
+          createResultTable(dataTable, i);
+          createResultChart(dataTable, i);
         }
-        for (i=0 ; i<obj.gen_infos.nres ; i++){
-          if(hash[obj.objs[i].infos.filepath] == undefined){
-            hash[obj.objs[i].infos.filepath] = new Array();
-          }
-          array = new Array();
-          for(j=0 ; j<obj.objs[i].infos.header.length ; j++){
-            if(array[obj.objs[i].infos.header[j]] == undefined || array[obj.objs[i].infos.header[j]] != 0){
-              array.push(obj.objs[i].data[obj.objs[i].infos.header[j]]);
-            }
-          }
-          hash[obj.objs[i].infos.filepath].push(array);
-        }
-        var count = 0;
-        for(i=0 ; i<Object.keys(hash).length ; i++){
-          pippo = new Array();
-          for(j=0 ; j<obj.gen_infos.nres ; j++){
-           if(obj.objs[j].infos.filepath == Object.keys(hash)[i]){
-              for(k=0 ; k<obj.objs[j].infos.header.length ; k++){
-                pippo.push({"title": obj.objs[j].infos.header[k]});
-              }
-              papa.push(pippo);
-              break;
-            }
-          }
-          if(count == 0){
-            $("<li class=\"active text-center\"><a href=\"#sectionResult_body_Content_"+count+"\" data-toggle=\"tab\" id=\"#sectionResult_body_Content_"+count+"\" onclick=\"switchContent(id)\">"+count+"</a></li>").appendTo('#sectionResult_body_Tab');
-            $("<div class=\"tab-pane fade in active\" id=\"sectionResult_body_Content_"+count+"\"></div>").appendTo('#sectionResult_body_Content');
-          }
-          else{
-            $("<li class=\"text-center\"><a href=\"#sectionResult_body_Content_"+count+"\" data-toggle=\"tab\" id=\"#sectionResult_body_Content_"+count+"\" onclick=\"switchContent(id)\">"+count+"</a></li>").appendTo('#sectionResult_body_Tab');
-            $("<div class=\"tab-pane fade in\" id=\"sectionResult_body_Content_"+count+"\"></div>").appendTo('#sectionResult_body_Content');
-          }
-          $("<div id=\"table_"+count+"\" class=\"myTable\"></div>").appendTo('#sectionResult_body_Content_'+count);
-          $("<div id=\"chart_"+count+"\" class=\"myChart\">"+count+"</div>").appendTo('#sectionResult_body_Content_'+count);
-          $("<div id=\"indexColumn_"+count+"\" class=\"myIndexColumn\"></div>").appendTo(SelectedFile_Body);
-          for(index=0;index<papa[count].length;index++){
-            $("<label class=\"checkbox\"><p><input type=\"checkbox\" id=\"indexColumn"+count+"_"+papa[count][index]['title']+"\">"+papa[count][index]['title']+"</label>").appendTo('#indexColumn_'+count);
-          }
-          if(count!=0){
-            $('#indexColumn_'+count).hide();
-          }
-          $("<table border=\"3\" class=\"table table-striped table-condensed\" id=\"example"+ count +"\"></table>").appendTo('#table_'+count);
-          $('#example'+count).dataTable({
-            "data": hash[Object.keys(hash)[count]],
-            "columns": papa[count],
-          });
-          count++;
-        }
-        arrayNaN=[];
-        supportoNaN=[];
-        indexGene=[];
-        rows=[];
-        charts=[];
-        dati=[];
-        for(i=0 ; i<papa.length ; i++){
-          arrayNaN=[];
-          for(k=0 ; k<hash[Object.keys(hash)[i]][0].length ; k++){
-            supportoNaN=[];
-            if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_short_name")==-1)){
-              supportoNaN.push(k);
-            }
-            else if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_short_name")!=-1)){
-              indexGene[i] = k;
-            }
-            if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_symbol")==-1)){
-              supportoNaN.push(k);
-            }
-            else if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_symbol")!=-1)){
-              indexGene[i] = k;
-            }
-          }
-          arrayNaN.push(supportoNaN); 
-        }
-        google.setOnLoadCallback(drawCharts()); 
         $('.myChart').hide();
         $('.myTable').show();
+        $('#sectionResult_body_Content_'+0).addClass('active');
         $('a[href="#Result"]').tab('show');
       }
+
+      function createResultTab(id, show_id ){
+        $("<li class=\"text-center\"><a href=\"#sectionResult_body_Content_"+id+"\" data-toggle=\"tab\" id=\"#sectionResult_body_Content_"+id+"\" onclick=\"switchContent(id)\">"+show_id+"</a></li>").appendTo('#sectionResult_body_Tab');
+        $("<div class=\"tab-pane fade in\" id=\"sectionResult_body_Content_"+id+"\"></div>").appendTo('#sectionResult_body_Content');        
+      }
+
+      function createResultTable(table, id){
+        $("<div id=\"table_"+id+"\" class=\"myTable\"></div>").appendTo('#sectionResult_body_Content_'+id);
+        $("<table border=\"3\" class=\"table table-striped table-condensed\" id=\"example"+ id +"\"></table>").appendTo('#table_'+id);
+        $('#example'+id).dataTable(table);
+      }
+
+      function createResultChart(table, id){
+        $("<div id=\"chart_"+id+"\" class=\"myChart\">"+id+"</div>").appendTo('#sectionResult_body_Content_'+id);
+      }
+
+
+      // function showTableChart(obj){
+
+      //   for(i=0 ; i<Object.keys(hash).length ; i++){
+  
+      //     if(count == 0){
+      //       $("<li class=\"active text-center\"><a href=\"#sectionResult_body_Content_"+count+"\" data-toggle=\"tab\" id=\"#sectionResult_body_Content_"+count+"\" onclick=\"switchContent(id)\">"+count+"</a></li>").appendTo('#sectionResult_body_Tab');
+      //       $("<div class=\"tab-pane fade in active\" id=\"sectionResult_body_Content_"+count+"\"></div>").appendTo('#sectionResult_body_Content');
+      //     }
+      //     else{
+      //       $("<li class=\"text-center\"><a href=\"#sectionResult_body_Content_"+count+"\" data-toggle=\"tab\" id=\"#sectionResult_body_Content_"+count+"\" onclick=\"switchContent(id)\">"+count+"</a></li>").appendTo('#sectionResult_body_Tab');
+      //       $("<div class=\"tab-pane fade in\" id=\"sectionResult_body_Content_"+count+"\"></div>").appendTo('#sectionResult_body_Content');
+      //     }
+      //     $("<div id=\"table_"+count+"\" class=\"myTable\"></div>").appendTo('#sectionResult_body_Content_'+count);
+      //     $("<div id=\"chart_"+count+"\" class=\"myChart\">"+count+"</div>").appendTo('#sectionResult_body_Content_'+count);
+      //     $("<div id=\"indexColumn_"+count+"\" class=\"myIndexColumn\"></div>").appendTo(SelectedFile_Body);
+      //     for(index=0;index<papa[count].length;index++){
+      //       $("<label class=\"checkbox\"><p><input type=\"checkbox\" id=\"indexColumn"+count+"_"+papa[count][index]['title']+"\">"+papa[count][index]['title']+"</label>").appendTo('#indexColumn_'+count);
+      //     }
+      //     if(count!=0){
+      //       $('#indexColumn_'+count).hide();
+      //     }
+      //   }
+      //   // arrayNaN=[];
+      //   // supportoNaN=[];
+      //   // indexGene=[];
+      //   // rows=[];
+      //   // charts=[];
+      //   // dati=[];
+      //   // for(i=0 ; i<papa.length ; i++){
+      //   //   arrayNaN=[];
+      //   //   for(k=0 ; k<hash[Object.keys(hash)[i]][0].length ; k++){
+      //   //     supportoNaN=[];
+      //   //     if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_short_name")==-1)){
+      //   //       supportoNaN.push(k);
+      //   //     }
+      //   //     else if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_short_name")!=-1)){
+      //   //       indexGene[i] = k;
+      //   //     }
+      //   //     if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_symbol")==-1)){
+      //   //       supportoNaN.push(k);
+      //   //     }
+      //   //     else if((isNaN(Number(hash[Object.keys(hash)[i]][0][k]))) && (papa[i][k]['title'].search("_symbol")!=-1)){
+      //   //       indexGene[i] = k;
+      //   //     }
+      //   //   }
+      //   //   arrayNaN.push(supportoNaN); 
+      //   // }
+      //   // google.setOnLoadCallback(drawCharts()); 
+      //   // $('.myChart').hide();
+      //   $('.myTable').show();
+      //   $('a[href="#Result"]').tab('show');
+      // }
 
       function drawCharts(){  //viene chiamata solo alla fine dell'esecuzione di tutta la funzione
         for(i=0 ; i<papa.length ; i++){
@@ -186,10 +220,11 @@ google.load('visualization', '1', {packages: ['corechart', 'bar']});
             
             charts[i] = new google.charts.Bar(document.getElementById('chart_'+i));
             charts[i].draw(dati[i], options);
-            console.log('i='+i+' chart made!');
+            // console.log(dati[i]);
+            // console.log('i='+i+' chart made!');
           }
           else{ //se indexGene[i] è undefined
-            console.log('i='+i+' chart Not made!');
+            // console.log('i='+i+' chart Not made!');
             $("<p>NoChart!</p>").appendTo('#chart_'+i);
           }
         }
@@ -222,9 +257,9 @@ google.load('visualization', '1', {packages: ['corechart', 'bar']});
       }
 
       function switchContent(id){
-        console.log(id);
+        // console.log(id);
         var index = id.substring(id.length-1,id.length);
-        console.log(index);
+        // console.log(index);
         $('.myIndexColumn').hide();
         $('#indexColumn_'+index).show();
       }
