c9b9a6b37f748166acbde4d5222828f97a29820b
komodojoe92 <g.troiano1@campus.unimib.it>
2015-06-24 16:54:43 +0200

    Update ui.js
    refactoring and improve dynamic chart's controll

--- nodetest1/public/js/ui.js
+++ nodetest1/public/js/ui.js
@@ -117,7 +117,7 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
       }
 
       function showTableChart(tables){
-        dati=tables;
+        dati = tables;
         createResults(tables);
       }
 
@@ -130,25 +130,25 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
         }
         // console.log(keys);
         // console.log(tables)
-        for (var i = 0; i < keys.length; i++) {
-          var tableName = keys[i];
-          // console.log(tableName);
+        $.each(keys, function(index,value){
+          var tableName = keys[index];
           var dataTable = tableToDataTable(tables[tableName]);
-          // console.log(dataTable);
-          createResultTab(i, i);
-          createResultTable(dataTable, i);
-          createResultChart(dataTable, i);
-          createColumnsSides(dataTable,i);
-          //drawGoogleChart(dataTable,i);
-        }
+          
+          createResultTab(index, index);
+          createResultTable(dataTable, index);
+          createResultChart(dataTable, index);
+          createColumnsSides(dataTable,index);
+        });
+        
         $('.myIndexColumn').hide();
         $('.myChart').hide();
+        
         $('#indexColumn_0').show();
         $('#sectionResult_body_Content_li_0').addClass('active');  
         $('#sectionResult_body_Content_0').addClass('active');
         $('#sectionResult_header_switchSection_table').addClass('active');       
+        
         $('a[href="#Result"]').tab('show');
-        // google.setOnLoadCallback(drawCharts()); 
       }
 
       function tableToDataTable(dataTable){
@@ -164,7 +164,7 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
         return dataTable.header.map(function(columnName){
           // console.log(columnName);
           return {title:columnName};
-        })
+        });
       }
 
       function createResultTab(id, show_id ){
@@ -175,77 +175,68 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
 
         var hSectionResult_body = hWindow-hNavBar-hSectionTab-hSectionResult_header-158;
 
-        $("<li class=\"text-center\" id=\"sectionResult_body_Content_li_"+id+"\"><a href=\"#sectionResult_body_Content_"+id+"\" data-toggle=\"tab\" id=\"sectionResult_body_Content"+id+"\" onclick=\"switchContent(id)\">"+show_id+"</a></li>").appendTo('#sectionResult_body_Tab');
-        $("<div class=\"tab-pane fade in\" style=\"overflow:auto; max-height:"+(hSectionResult_body)+"px\" id=\"sectionResult_body_Content_"+id+"\"></div>").appendTo('#sectionResult_body_Content');        
+        $("<li class=\"text-center\" id=\"sectionResult_body_Content_li_"+ id +"\"><a href=\"#sectionResult_body_Content_"+ id +"\" data-toggle=\"tab\" id=\"sectionResult_body_Content"+ id +"\" onclick=\"switchContent(id)\">"+ show_id +"</a></li>").appendTo('#sectionResult_body_Tab');
+        $("<div class=\"tab-pane fade in\" style=\"overflow:auto; max-height:"+ hSectionResult_body +"px\" id=\"sectionResult_body_Content_"+ id +"\"></div>").appendTo('#sectionResult_body_Content'); 
       }
 
       function createResultTable(table, id){
-        $("<div id=\"table_"+id+"\" class=\"myTable\" style=\"overfloaw:auto ; height:100%\"></div>").appendTo('#sectionResult_body_Content_'+id);
-        $("<table border=\"3\" class=\"table table-striped table-condensed\" id=\"example"+ id +"\"></table>").appendTo('#table_'+id);
-        $('#example'+id).dataTable(table);
+        $("<div id=\"table_"+id+"\" class=\"myTable\" style=\"overfloaw:auto ; height:100%\"></div>").appendTo('#sectionResult_body_Content_'+ id);
+        $("<table border=\"3\" class=\"table table-striped table-condensed\" id=\"example"+ id +"\"></table>").appendTo('#table_'+ id);
+        $('#example'+ id).dataTable(table);
       }
 
       function createResultChart(table, id){
-        $("<div id=\"chart_"+id+"\" class=\"myChart\" style=\"height:100% ; width:100%\"></div>").appendTo('#sectionResult_body_Content_'+id);
+        $("<div id=\"chart_"+ id +"\" class=\"myChart\" style=\"height:100% ; width:100%\"></div>").appendTo('#sectionResult_body_Content_'+ id);
       }
 
       function createColumnsSides(table, id){
-
-          $("<div id=\"indexColumn_"+id+"\" class=\"myIndexColumn\" style=\"margin-left:8px;\"></div>").appendTo('#SelectedColumns_Body');
-          for(index=0;index<table.columns.length; index++){
-            $("<label class=\"checkbox\"><p class=\"myIndexCheckbox\"><input type=\"checkbox\" id=\"indexColumn_"+id+"_"+table.columns[index].title+"_"+index+"\" onclick=\"hideShowColumnTable(id)\">"+table.columns[index].title+"</label>").appendTo('#indexColumn_'+id);
-            $('#indexColumn_'+id+'_'+table.columns[index].title+"_"+index).prop('checked', true);
-          }
+          $("<div id=\"indexColumn_"+ id +"\" class=\"myIndexColumn\" style=\"margin-left:8px;\"></div>").appendTo('#SelectedColumns_Body');
+          
+          $.each(table.columns, function(index, value){
+            $("<label class=\"checkbox\"><p class=\"myIndexCheckbox\"><input type=\"checkbox\" id=\"indexColumn_"+ id +"_"+ value.title +"_"+ index +"\" onclick=\"hideShowColumnTable(id)\">"+ value.title +"</label>").appendTo('#indexColumn_'+ id);
+            $('#indexColumn_'+ id +'_'+value.title +"_"+ index).prop('checked', true);
+          });
       }
   
       function hideShowColumnTable(id){
-        var numberOfTable=id.split("_")[1];
-        var indexCol=id.substring(id.length-2);
-        var i=indexCol.indexOf("_");
-        if (i > -1){
-          indexCol=indexCol.split("_")[1];
-        }
-        var table = $('#example'+numberOfTable).DataTable();
+        var numberOfTable = id.split("_")[1];
+        var indexCol = id.substring(id.length-2);
+        var index = indexCol.indexOf("_");
+        var table = $('#example'+ numberOfTable).DataTable();
         
-        if( $('#'+id).prop("checked") ){
-          table.column( indexCol ).visible( true );
+        if (index > -1){
+          indexCol = indexCol.split("_")[1];
+        }
+        
+        if( $('#'+ id).prop("checked") ){
+          table.column(indexCol).visible( true );
         }
         else{
-          table.column( indexCol ).visible( false );
+          table.column(indexCol).visible( false );
         }
         table.columns.adjust().draw();
 
         if( $('.myChart').is(':visible') ){
-          var idForSwitchContent = "Content"+id.split("_")[4];
           switchTableChart(id);
         }
       }
 
       function drawGoogleChart(table, id){
-        //var columnIndexes = filterColumnsForChart(table, id);
         //dal server, insieme ai dati viene inviato per ciascun grafico, l'indice dell'elemento di riferimento per l'asse x ed la sottostringa degli elementi da graficare nel chartS
         var keys = Object.keys(dati);
         var tableName = keys[id];
-        /*if(dati[tableName]['stringColumns'] == undefined){
-          alert('input Error! Please enter a valid column\'s name');
-          return;
-        }*/
-        var columnIndexes = filterColumnsForChart(table, id, dati[tableName]['hAxis'], dati[tableName]['stringColumns']);
 
-        var header = generateHeaderForChart(table, id, columnIndexes);
-
-        var rows = generateRowsForChart(table, id, columnIndexes) ;
-        
-        rows = changeRowsFromStringToNumber(rows);
-        
-        var googleDataTable = dataTableToGoogleChartDataTable(header, rows, id);
-        var hSelectedColumns_Body = calcHeightWindow('#SelectedColumns_Body');
         var hsectionResult_body_Tab = calcHeightWindow('#sectionResult_body_Tab');
-        
-        var hChart = $('#listingTag_Body').css('max-height').split("px")[0]-hsectionResult_body_Tab-20;
-        var wChart = $('#sectionResult_body_Content').width()-20;
+        var hMaxListingTag_Body = $('#listingTag_Body').css('max-height');
 
-        var tableName = Object.keys(dati)[0];
+        var columnIndexes = filterColumnsForChart(table, id, dati[tableName]['hAxis'], dati[tableName]['stringColumns']);
+        var header = generateHeaderForChart(table, id, columnIndexes);
+        var rows = generateRowsForChart(table, id, columnIndexes) ;
+        rows = changeRowsFromStringToNumber(rows);
+        var googleDataTable = dataTableToGoogleChartDataTable(header, rows, id);
+        
+        var hChart = hMaxListingTag_Body.split("px")[0] - hsectionResult_body_Tab - 20;
+        var wChart = $('#sectionResult_body_Content').width() - 20;
 
         var options = {
               chart: {
@@ -258,21 +249,20 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
                       }
               };
             
-        chart = new google.charts.Bar(document.getElementById('chart_'+id));
+        chart = new google.charts.Bar(document.getElementById('chart_'+ id));
         chart.draw(googleDataTable, options);
-
       }
 
-      function filterColumnsForChart(table, id, indexVaxis, substring){
+      function filterColumnsForChart(table, id, indexHaxis, substring){
         var columnIndexesChecked = [];
-        columnIndexesChecked[0]=indexVaxis;
-        var keys = Object.keys(dati);
-        var tableName = keys[id];
+        columnIndexesChecked[0] = indexHaxis;
 
-        $('#indexColumn_'+id).children().children().children().each(function(index,value){
-          //console.log(value.id);
-          //console.log(substring);
-          if((value.checked) && ((value.id).search(substring)!= -1) && !isNaN(Number(table['data'][0][index] ) ) ){
+        $('#indexColumn_'+ id).children().children().children().each( function(index,value){
+          var substringIsInValue = ( (value.id).search(substring)!= -1 );
+          var numericalDataValue = ( Number(table['data'][0][index] ) );
+          var numericalDataValueIsNan = isNaN(numericalDataValue);
+          
+          if( (value.checked) && (substringIsInValue) && (!numericalDataValueIsNan) ){
             columnIndexesChecked.push(index);
           }
         });
@@ -281,9 +271,9 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
 
       function generateHeaderForChart(table, id, columnIndexes){
         var header = [];
-        for(i=0; i<columnIndexes.length; i++){
-          header.push(table.columns[columnIndexes[i]].title);
-        }
+        $.each(columnIndexes, function(index,value){
+          header.push(table.columns[value].title);
+        });
         return header;
       }
 
@@ -333,91 +323,115 @@ google.load('visualization', '1.1', {packages: ['corechart', 'bar']});
         var keys = Object.keys(dati);
         var tableName = keys[index];
         var dataTable = tableToDataTable(dati[tableName]);
+
         var hAxis = dati[tableName]['hAxis'];
         var stringColumns = dati[tableName]['stringColumns'];
-
+        
         $('.myIndexColumn').hide();
         $('.myTable').hide();
         $('.myChart').hide();
 
-        if( $('#sectionResult_header_switchSection_chart').is('.active') && (hAxis!=undefined) && (stringColumns!=undefined) ){
-          $('#chart_'+index).show();
-          $('#SelectColumnsForChart_Body_HaxisForm_Warning').empty();
-          $('#SelectColumnsForChart_Body_ColumnsForm_Warning').empty();
-          $('#SelectColumnsForChart_Body_HaxisForm').val(dati[tableName]['header'][hAxis]);
-          $('#SelectColumnsForChart_Body_ColumnsForm').val(stringColumns);
-          //console.log('chart_'+index);
+        if( $('#sectionResult_header_switchSection_chart').is('.active') && (hAxis != undefined) && (stringColumns != undefined) ){
+          $('#chart_'+ index).show();
           $(".myIndexCheckbox").show();
-          filterDivColumnForChart(dati[tableName],index,hAxis,stringColumns);
+          setEmptyWarning();
+          setValueInSelectColumnsForChart(dati[tableName].header[hAxis], stringColumns);
+          filterDivColumnForChart(dati[tableName], index, hAxis, stringColumns);
           drawGoogleChart(dataTable,index);
         }
         else if ($('#sectionResult_header_switchSection_table').is('.active')){
           //console.log('table');
           $('#table_'+index).show();
           $('.myIndexCheckbox').show();
-          $('#SelectColumnsForChart_Body_HaxisForm_Warning').empty();
-          $('#SelectColumnsForChart_Body_ColumnsForm_Warning').empty();
-          $('#SelectColumnsForChart_Body_HaxisForm').val('');
-          $('#SelectColumnsForChart_Body_ColumnsForm').val('');      
+
+          setEmptyWarning();
+          setValueInSelectColumnsForChart('', '');      
         }
         else{
-          $('#SelectColumnsForChart_Body_HaxisForm').val('');
-          $('#SelectColumnsForChart_Body_ColumnsForm').val('');
+          setValueInSelectColumnsForChart('', '');
           sendStringsForChart();
         }
-        $('#indexColumn_'+index).show();
+        $('#indexColumn_'+ index).show();
       }
 
-      function filterDivColumnForChart(table, id, indexVaxis, substring){
+      function setValueInSelectColumnsForChart(hAxisForm,ColumnsForm){
+        $('#SelectColumnsForChart_Body_HaxisForm').val(hAxisForm);
+        $('#SelectColumnsForChart_Body_ColumnsForm').val(ColumnsForm);  
+      }
+
+      function setEmptyWarning(){
+        $('#SelectColumnsForChart_Body_HaxisForm_Warning').empty();
+        $('#SelectColumnsForChart_Body_ColumnsForm_Warning').empty();
+      }
+
+      function filterDivColumnForChart(table, id, indexHaxis, substring){
         var columnIndexesChecked = [];
         var fistRowData =  table['data'][0];
-        columnIndexesChecked[0]=indexVaxis;
+        columnIndexesChecked[0] = indexHaxis;
 
-        $('#indexColumn_'+id).children().children().children().each(function(index,value){
+        $('#indexColumn_' +id).children().children().children().each( function(index,value){
           var firstRowDataElementNumeric = Number(fistRowData[index]);
-          if((value.id).search(substring) == -1 || (isNaN(firstRowDataElementNumeric)) ){
+          if( ((value.id).search(substring) == -1) || (isNaN(firstRowDataElementNumeric)) ){
             $(value).parent().hide();
           }
         });
       }
 
       function sendStringsForChart(){
-        var stringHaxis = "";
-        var stringColumns ="";
         var idSectionActive= $('#sectionResult_body_Tab').children('.active').attr('id');
-        var numberOfTable = idSectionActive.split("_")[4];
+        var index = idSectionActive.split("_")[4];
         var keys = Object.keys(dati);
-        var filepath = keys[numberOfTable];
-        $('#SelectColumnsForChart_Body_HaxisForm_Warning').empty();
-        $('#SelectColumnsForChart_Body_ColumnsForm_Warning').empty();
-        //console.log(filepath);
-        setVaxisValue(filepath);
-
-        setStringOfColumns(filepath);
+        var tableName = keys[index];
+        //dati[tableName]['hAxis'] = undefined;
+        //dati[tableName]['stringColumns'] = undefined;
+        
+        setEmptyWarning();
+        
+        setHaxisValue(tableName);
+        setStringOfColumns(tableName);
       }
 
-      function setVaxisValue(filepath){
-        if($('#SelectColumnsForChart_Body_HaxisForm').val() != "" ){
+      function setHaxisValue(filepath){
+        var stringHaxis = "";
+
+        if( $('#SelectColumnsForChart_Body_HaxisForm').val() != "" ){
           stringHaxis = $('#SelectColumnsForChart_Body_HaxisForm').val();
           stringHaxis = stringHaxis.split(" ")[0];
           var index = (dati[filepath]['header']).indexOf(stringHaxis);
-          if(isNaN(Number(dati[filepath]['data'][0][index]))){
+
+          if( (index != -1) & isNaN(Number(dati[filepath]['data'][0][index])) ){
             dati[filepath]['hAxis'] = index;
           }
-          //console.log(dati[filepath]['hAxis']);
+          else{
+            $('#SelectColumnsForChart_Body_HaxisForm_Warning').text("*Not valid input.");
+          }
         }
+
         else{
           $('#SelectColumnsForChart_Body_HaxisForm_Warning').text("*missing input.");
         }
       }
 
       function setStringOfColumns(filepath){
-        if($('#SelectColumnsForChart_Body_ColumnsForm').val() != "" ){
+        var stringColumns = "";
+        if( $('#SelectColumnsForChart_Body_ColumnsForm').val() != "" ){
           stringColumns = $('#SelectColumnsForChart_Body_ColumnsForm').val();
           stringColumns = stringColumns.split(" ")[0];
-          dati[filepath]['stringColumns'] =  stringColumns;
-          //console.log(dati[filepath]['stringColumns']);
-          switchTableChart('sectionResult_header_switchSection_chart');
+          var stringOfHaxis = $('#SelectColumnsForChart_Body_HaxisForm').val();
+          /*console.log(stringOfHaxis);
+          console.log(stringColumns);
+          $.each(dati[filepath]['header'], function(index,value){
+            console.log(index);
+            console.log(value);
+          });
+          */
+          if(stringColumns != stringOfHaxis){
+            dati[filepath]['stringColumns'] = stringColumns;
+            switchTableChart('sectionResult_header_switchSection_chart');
+          }
+          else{
+            $('#SelectColumnsForChart_Body_ColumnsForm_Warning').text("*Not valid input.");
+          }
         }
         else{
           $('#SelectColumnsForChart_Body_ColumnsForm_Warning').text("*missing input.");
