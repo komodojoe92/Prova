1a72cd81d0e13decc0f5b3d757d5726fc1e5a325
komodojoe92 <g.troiano1@campus.unimib.it>
2015-07-01 14:28:08 +0200

    Update index.js

--- Kale/routes/index.js
+++ Kale/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) 
 {
-  res.render('index', {webserver: "http://0.0.0.0:3000"});
+  res.render('index', {webserver: "http://localhost:3000"});
 });
 
 module.exports = router;
