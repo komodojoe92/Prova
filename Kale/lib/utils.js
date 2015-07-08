4c40c29957d5aeb901544f8be9d0c25b1cf1f690
Giovanni Troiano <g.troiano1@campus.unimib.it>
2015-07-08 12:25:26 +0200

    Update utils.js

--- Kale/lib/utils.js
+++ Kale/lib/utils.js
@@ -11,9 +11,9 @@ module.exports = {
     if( resultsLength != 0){
       //iterate over the objs array which contains {:infos, :data}
       var filepath = '';
-      
       var record = undefined;
       var header_name = undefined;
+      
       for (i=0 ; i<resultsLength ; i++){
         record = bdfResultJson.objs[i];
         filepath = record.infos.filepath;
@@ -23,8 +23,8 @@ module.exports = {
           
           h['data'] = [];
           h['header'] = record.infos.header;
-          h['hAxis'] = 3;
-          h['stringColumns'] = "FPKM";
+          h['hAxis'] = 3;     //it sets the default value of hAxis
+          h['stringColumns'] = "FPKM";    //it sets the default value of stringColumns
           dataTables[filepath] = h;
         }
 
@@ -37,7 +37,36 @@ module.exports = {
         dataTables[filepath]['data'].push(row);
       }
     } //if bdfResultJson != 0
+
+    createHeaderStringAndHeaderNumeric(dataTables);
+
     return dataTables;
   }
 
 }; //module.exports
+
+/* THIS FUNCTION CONTROLLS IF HEADERS OF TABLES HAVE TYPE OF VALUES EQUAL TO 'STRING' OR 'NUMERIC' AND IT MAKES DIFFERENT VARIBALES FOR BOTH TYPE */
+function createHeaderStringAndHeaderNumeric(dataTables){
+  var keys = Object.keys(dataTables);
+
+    for (i=0 ; i<keys.length ; i++){
+      var tableName = keys[i];
+      var table = dataTables[tableName];
+      var firstRow = table['data'][0];  //it controls the first row of the table
+      var isString = [];
+      var isNumeric = [];
+
+      for (j=0 ; j<firstRow.length ; j++){  //for each value in firstRow it controls if value is String or Numeric
+        var elementNumeric = Number(firstRow[j]);
+
+        if ( isNaN(elementNumeric) && (firstRow[j] != "OK") && (firstRow[j] != "-") ){
+          isString.push(table['header'][j]);
+        }
+        else if ( !isNaN(elementNumeric) ){
+          isNumeric.push(table['header'][j]);
+        }
+      }
+      table['headerString'] = isString;
+      table['headerNumeric'] = isNumeric;
+    }
+}
